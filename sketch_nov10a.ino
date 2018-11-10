0ecce40e1c585c7fa2b81618695530c888416e7b
tlincke125 <43426648+tlincke125@users.noreply.github.com>
2018-11-10 15:27:48 -0700

    Add files via upload

--- sketch_nov10a.ino
+++ sketch_nov10a.ino
@@ -1,41 +1,80 @@
+
 #include <SoftwareSerial.h>
-//* RX is digital pin 10 (connect to TX of other device - iRobot DB25
-// pin 2)
-//* TX is digital pin 11 (connect to RX of other device - iRobot DB25
-// pin 1)
-#define rxPin 0
-#define txPin 1
-// set up a new software serial port:
-SoftwareSerial softSerial = SoftwareSerial(rxPin, txPin);
-int inByte = 0; // incoming serial byte
-/*************************************************************
-SETUP
-*************************************************************/
-void setup()
-{
-delay(2000); // Needed to let the robot initialize
-// define pin modes for software tx, rx pins:
- pinMode(rxPin, INPUT);
- pinMode(txPin, OUTPUT);
-// start the the SoftwareSerial port 57600 bps (robot’s default)
- softSerial.begin(115200);
-// start hardware serial port
-  Serial.begin(115200);
-  softSerial.write(128); // This command starts the OI.
-  softSerial.write(131); // set mode to safe (see p.7 of OI manual)
+
+int rxPin = 3;
+int txPin = 4;
+int ledPin = 13;
+
+SoftwareSerial Roomba(rxPin,txPin);
+
+#define bumpright (sensorbytes[0] & 0x01)
+#define bumpleft  (sensorbytes[0] & 0x02)
+
+void setup() {
+ pinMode(ledPin, OUTPUT);   // sets the pins as output
+ Serial.begin(19200);
+ Roomba.begin(19200);  
+ digitalWrite(ledPin, HIGH); // say we're alive
+ Serial.println ("Sending start command...");
+ delay (1000);
+  // set up ROI to receive commands  
+ Roomba.write(128);  // START
+ delay(150);
+ Serial.println ("Sending Safe Mode command...");
+ delay (1000);
+ Roomba.write(131);  // CONTROL
+ delay(150);
+ digitalWrite(ledPin, LOW);  // say we've finished setup
+ Serial.println ("Ready to go!");
+ delay (5000);
 }
-/*************************************************************
-LOOP
-*************************************************************/
-void loop()
-{
- //serial.write(ana
- softSerial.write(142); // requests the OI to send a packet of
- // sensor data bytes
- softSerial.write(3); // request cliff sensor value specifically
- delay(250); // poll sensor 4 times a second
- if (softSerial.available() > 0) {
- inByte = softSerial.read();
- }
- Serial.println(inByte);
+
+void loop() {
+ digitalWrite(ledPin, HIGH); // say we're starting loop
+ Serial.println ("Go Forward");
+ goForward();
+ delay (500);
+ Serial.println ("Halt!");
+ halt();
+ Serial.println ("Go Backwards");
+ delay (500);
+ goBackward();
+ delay (500);
+ Serial.println ("Halt!");
+ halt();
+ while(1) { } // Stop program
+}
+
+void goForward() {
+ Roomba.write(137);   // DRIVE
+ Roomba.write((byte)0x00);   // 0x00c8 == 200
+ Roomba.write(0xc8);
+ Roomba.write(0x80);
+ Roomba.write((byte)0x00);
+}
+
+
+
+void goBackward() {
+ Roomba.write(137);   // DRIVE
+ Roomba.write(0xff);   // 0xff38 == -200
+ Roomba.write(0x38);
+ Roomba.write(0x01);
+ Roomba.write((byte)0xF4);
+}
+
+void spin(){
+
+
+
+  
+}
+
+void halt(){
+byte j = 0x00;
+Roomba.write(137);   
+Roomba.write(j);   
+Roomba.write(j);
+Roomba.write(j);
+Roomba.write(j);
 }
